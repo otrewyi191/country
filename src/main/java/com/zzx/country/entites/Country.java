a98771c4db366333e860995e95208b0ee991abab
zzx <piofcircle@gmail.com>
2018-03-25 20:22:15 +0800

    entity

--- src/main/java/com/zzx/country/entites/Country.java
+++ src/main/java/com/zzx/country/entites/Country.java
@@ -1,6 +1,15 @@
 package com.zzx.country.entites;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Country {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
     private String name;
 
     @Override
