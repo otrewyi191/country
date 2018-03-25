90f462f4203890631d4b453159b148dabb1b5e17
zzx <piofcircle@gmail.com>
2018-03-25 22:42:27 +0800

    New Button

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,13 +7,26 @@ $(document).ready(function () {
 
         event.preventDefault();
         var href = $(this).attr('href');
+        var text = $(this).text();
 
-        $.get(href, function (country, status) {
-            $('.myForm #id').val(country.id);
-            $('.myForm #name').val(country.name);
-            $('.myForm #capital').val(country.capital);
-        });
+        if(text=="Edit"){
 
-        $('.myForm #exampleModal').modal();
+
+            $.get(href, function (country, status) {
+                $('.myForm #id').val(country.id);
+                $('.myForm #name').val(country.name);
+                $('.myForm #capital').val(country.capital);
+            });
+
+            $('.myForm #exampleModal').modal();
+        }
+        else {
+            $('.myForm #id').val('');
+            $('.myForm #name').val('');
+            $('.myForm #capital').val('');
+
+            $('.myForm #exampleModal').modal();
+        }
+
     });
 });
